6c31fe7debe1ea1bd6a058c96c96062514318869
Benjamin Klein <ben@silver-chalice.com>
2011-03-16 06:20:53 -0500

    steal uploadBlob code for use in publication create. the register of his burial was signed by the clergyman, the clerk, the undertaker, and the chief mourner.

--- war/WEB-INF/groovy/uploadBlob.groovy
+++ war/WEB-INF/groovy/uploadBlob.groovy
@@ -1,10 +1,20 @@
+import com.google.appengine.api.blobstore.BlobKey
+import com.google.appengine.api.datastore.Entity
+
 def blobs = blobstore.getUploadedBlobs(request)
-def blobKey = blobs["myTextFile"]
+def blobKey = blobs["cover"]
 
 response.status = 302
 
+def publication = new Entity("publication")
+publication.title = params.title
+publication.author = params.author
+publication.coverKey = blobKey.keyString
+
+publication.save()
+
 if (blobKey) {
-	redirect "/success?key=${blobKey.keyString}"	
+	redirect "publication/${publication.key.id}"
 } else {
 	redirect "/failure"
 }
