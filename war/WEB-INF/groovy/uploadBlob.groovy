582c67f28ce5545989f0aeb0199a6b91bbedfe5c
Benjamin Klein <ben@silver-chalice.com>
2011-03-23 15:10:42 -0500

    adding pdf blob to publication-saving groovlet. i might have been inclined, myself, to regard a coffin-nail as the deadest piece of ironmongery in the trade.

--- war/WEB-INF/groovy/uploadBlob.groovy
+++ war/WEB-INF/groovy/uploadBlob.groovy
@@ -2,18 +2,20 @@ import com.google.appengine.api.blobstore.BlobKey
 import com.google.appengine.api.datastore.Entity
 
 def blobs = blobstore.getUploadedBlobs(request)
-def blobKey = blobs["cover"]
+def blobKey1 = blobs["cover"]
+def blobKey2 = blobs["pdf"]
 
 response.status = 302
 
 def publication = new Entity("publication")
 publication.title = params.title
 publication.author = params.author
-publication.coverKey = blobKey.keyString
+publication.coverKey = blobKey1.keyString
+publication.pdfKey = blobKey2.keyString
 
 publication.save()
 
-if (blobKey) {
+if (blobKey1) {
 	redirect "publication/${publication.key.id}"
 } else {
 	redirect "/failure"
