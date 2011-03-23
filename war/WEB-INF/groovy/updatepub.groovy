326abf1333d0e98ad8699c44c58dd2259a6f30ed
Benjamin Klein <ben@silver-chalice.com>
2011-03-23 15:21:50 -0500

    add groovlet for publication update (after edit). there is no doubt that Marley was dead.

---
+++ war/WEB-INF/groovy/updatepub.groovy
@@ -0,0 +1,42 @@
+import com.google.appengine.api.datastore.KeyFactory
+import com.google.appengine.api.blobstore.BlobKey
+import com.google.appengine.api.datastore.Entity
+
+log.info "update publication"
+
+log.info "params are " + params
+
+if (params["id"]) {
+ def id = Long.parseLong(params["id"])
+ try {
+   def key = KeyFactory.createKey("publication", id)
+   def publication = datastore.get(key)
+
+   def blobs = blobstore.getUploadedBlobs(request)
+   def blobKey1 = blobs["cover"]
+   def blobKey2 = blobs["pdf"]
+
+   response.status = 302
+
+   publication.title = params.title
+   publication.author = params.author
+
+   if(blobKey1){
+     publication.coverKey = blobKey1.keyString
+   }
+
+   if(blobKey2){
+     publication.pdfKey = blobKey2.keyString
+   }
+
+   publication.save()   
+   redirect "/publishing"
+
+   } catch (Throwable t) {
+    log.info "here is some tea. " + t
+    redirect "/publishing"
+   }
+} else { 
+   log.info "dr@t"
+   redirect	"/publishing"
+}
