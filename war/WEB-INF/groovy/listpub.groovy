b6f4d354893c7002759ce95e2cf4ca6f6d05a2ae
Benjamin Klein <ben@silver-chalice.com>
2011-03-23 15:07:44 -0500

    no limit for publications listed. i don't mean to say that i know, of my own knowledge, what there is particularly dead about a door-nail.

--- war/WEB-INF/groovy/listpub.groovy
+++ war/WEB-INF/groovy/listpub.groovy
@@ -1,11 +1,11 @@
 import com.google.appengine.api.datastore.Query
-import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit
+import static com.google.appengine.api.datastore.FetchOptions.Builder.*
 
 try {
  def query = new Query("publication")
  query.addSort("title", Query.SortDirection.DESCENDING)
  def preparedQuery = datastore.prepare(query)
- def publications = preparedQuery.asList( withLimit(10) )
+ def publications = preparedQuery.asList( withOffset(0) )
 
  request.setAttribute "publications", publications
 forward "listpub.gtpl"
