fd9e060e61e20b5971e01c993064473061fbed71
Benjamin Klein <ben@silver-chalice.com>
2011-03-23 16:30:26 -0500

    remove redundant publication create groovlet. the firm was known as Scrooge and Marley.

--- war/WEB-INF/groovy/createpub.groovy
+++
@@ -1,9 +0,0 @@
-import com.google.appengine.api.datastore.Entity
-
-def publication = new Entity("publication")
-publication.title = params.title
-publication.author = params.author
-
-publication.save()
-
-redirect "publication/${publication.key.id}"
