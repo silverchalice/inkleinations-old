ca03171bf2b9df5c77e29caf20b97c239bc984dd
Benjamin Klein <ben@silver-chalice.com>
2011-03-24 11:20:49 -0500

    add publication pageCount. sometimes people new to the business called Scrooge Scrooge, and sometimes Marley, but he answered to both names.

--- war/WEB-INF/groovy/uploadBlob.groovy
+++ war/WEB-INF/groovy/uploadBlob.groovy
@@ -1,22 +1,23 @@
 import com.google.appengine.api.blobstore.BlobKey
 import com.google.appengine.api.datastore.Entity
 
-def blobs = blobstore.getUploadedBlobs(request)
-def blobKey1 = blobs["cover"]
-def blobKey2 = blobs["pdf"]
+def files = blobstore.getUploadedBlobs(request)
+def cover = files["cover"]
+def pdf = files["pdf"]
 
 response.status = 302
 
 def publication = new Entity("publication")
 publication.title = params.title
 publication.author = params.author
+publication.pageCount = params.pageCount
 publication.description = params.description
-publication.coverKey = blobKey1.keyString
-publication.pdfKey = blobKey2.keyString
+publication.coverKey = cover.keyString
+publication.pdfKey = pdf.keyString
 
 publication.save()
 
-if (blobKey1) {
+if (cover) {
 	redirect "publication/${publication.key.id}"
 } else {
 	redirect "/failure"
